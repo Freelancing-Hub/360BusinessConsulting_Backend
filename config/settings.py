c9edbecad93c3358ac11249bb4d0264ee46564ef
Freelancing-Hub <freelancehub.online@gmail.com>
2026-03-28 15:24:34 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-c%0ly2+9*#q-h()6824qq5z9a2q#dvr6y29t&s6q&ntijglyye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["three60businessconsulting-backend-1.onrender.com","https://360businessconsulting.netlify.app/"]
+ALLOWED_HOSTS = ["three60businessconsulting-backend-1.onrender.com"]
 
 # Application definition
 
@@ -140,3 +140,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True  
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://360businessconsulting.netlify.app/"
+]
